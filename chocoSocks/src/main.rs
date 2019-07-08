e6ce7ecbb1a0125b7db505803b13ec70dc8fca30
EvaMolotow <evamolotow@zeroscience.mk>
2019-07-09 01:08:45 +0200

    UDP initial commit - testing functionality

--- chocoSocks/src/main.rs
+++ chocoSocks/src/main.rs
@@ -1,6 +1,6 @@
 pub mod pcap;
 
-use std::net::{TcpListener, TcpStream, SocketAddr, Ipv4Addr, IpAddr, Shutdown};
+use std::net::{TcpListener, TcpStream, UdpSocket, SocketAddr, Ipv4Addr, IpAddr, Shutdown};
 use std::{thread,time};
 use std::time::{UNIX_EPOCH, SystemTime};
 use std::io::{Read, Write};
@@ -149,19 +149,50 @@ fn handle_client(mut client_stream : TcpStream)
 
 fn main() 
 {
-	let listener = match TcpListener::bind("127.0.0.1:80") 
+
+/* ================== Command listener ================== */
+
+	let mut command : [u8; 1024] = [0; 1024];
+	let command_listener = match UdpSocket::bind("127.0.0.1:1001")
 	{
 		Ok(v) => v,
-		Err(_) => panic!("Failed to open listener."),
+		Err(_) => panic!("Failed to open command UDP listener."),
 	};
+	
+	loop 
+	{
+		let (number_of_bytes, src_addr) = command_listener.recv_from(&mut command).expect("Didn't receive command data.");
 		
-	for stream in listener.incoming() 
+		match &command[0..2] 
+		{
+			&[0x30,0x0a] => println!("first command"),
+			&[0x31,0x0a] => println!("second command"),
+			&[0x32,0x0a] => println!("third command"),
+			_ => println!("Unknown command"),
+		}
+	}
+	
+/* ================== TCP listener ================== */
+
+	let tcp_listener = match TcpListener::bind("127.0.0.1:80") 
+	{
+		Ok(v) => v,
+		Err(_) => panic!("Failed to open TCP listener."),
+	};
+	
+	for stream in tcp_listener.incoming() 
 	{
 		let thread = thread::spawn(move || 
 			{
 				handle_client(stream.expect("Connection failed"));
 			});
 	}
+
+/* ================== UDP listener ================== */
 	
-		
+	let udp_listener = match UdpSocket::bind("127.0.0.1:81")
+	{
+		Ok(v) => v,
+		Err(_) => panic!("Failed to open UDP listener."),
+	};
 }
