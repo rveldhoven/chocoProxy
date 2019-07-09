05752837ea5814ed3de4811c41322ab8d0eb600c
fake <fake@fake.com>
2019-07-09 16:23:25 -0700

    Added first GUI mockups for new GUI.

--- chocoGUI/MainWindow.xaml.cs
+++ chocoGUI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace chocoGUI
 {
@@ -20,9 +21,52 @@ namespace chocoGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        public DispatcherTimer ui_dispatcher_timer = new DispatcherTimer() ;
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            cGlobalState.background_thread_start();
+
+            ui_dispatcher_timer.Tick += new EventHandler(ui_update_tick);
+            ui_dispatcher_timer.Interval = new TimeSpan(0, 0, 1);
+            ui_dispatcher_timer.Start();
+        }
+
+        private void ui_update_tick(object sender, EventArgs e)
+        {
+            var tcp_streams = cGlobalState.ui_tcp_streams_get();
+
+            var gridView = new GridView();
+
+            gridView.Columns.Add(new GridViewColumn() { Header = "Source process", DisplayMemberBinding = new Binding("SourceProcess") });
+            gridView.Columns.Add(new GridViewColumn() { Header = "Source IP", DisplayMemberBinding = new Binding("SourceIP") });
+            gridView.Columns.Add(new GridViewColumn() { Header = "Destination IP", DisplayMemberBinding = new Binding("DestinationIP") });
+            gridView.Columns.Add(new GridViewColumn() { Header = "Source port", DisplayMemberBinding = new Binding("SourcePort") });
+            gridView.Columns.Add(new GridViewColumn() { Header = "Destination port", DisplayMemberBinding = new Binding("DestinationPort") });
+            gridView.Columns.Add(new GridViewColumn() { Header = "Stream start", DisplayMemberBinding = new Binding("StreamStart") });
+            gridView.Columns.Add(new GridViewColumn() { Header = "Proxy connected", DisplayMemberBinding = new Binding("ProxyConnected") });
+
+            tcp_stream_view.View = gridView;
+
+            foreach (var tcp_stream in tcp_streams)
+            {
+                object stream_item = new
+                {
+                    SourceProcess = tcp_stream.source_process_name,
+                    SourceIP = tcp_stream.source_ip,
+                    DestinationIP = tcp_stream.destination_ip,
+                    SourcePort = tcp_stream.source_port,
+                    DestinationPort = tcp_stream.destination_port,
+                    StreamStart = tcp_stream.stream_start,
+                    ProxyConnected = (tcp_stream.proxy_connected == true ? "yes" : "no")
+                };
+
+                if (tcp_stream_view.Items.Contains(stream_item) == false)
+                    tcp_stream_view.Items.Add(stream_item); 
+            }
         }
     }
 }
---
+++ chocoGUI/cGlobalState.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace chocoGUI
+{
+    public class cTCPStream
+    {
+        public string source_process_pid { get; set; }
+        public string source_process_name { get; set; }
+        public string source_ip { get; set; }
+        public string destination_ip{ get; set; }
+        public string source_port{ get; set; }
+        public string destination_port{ get; set; }
+        public string backend_file{ get; set; }
+        public bool proxy_connected { get; set; }
+        public string stream_start { get; set; }
+    }
+    static class cGlobalState
+    {
+        private static object           _tcp_streams_mutex = new object();
+        private static List<cTCPStream> _tcp_streams = new List<cTCPStream>();
+
+        private static Thread _background_thread;
+        private static bool _background_is_running = false;
+
+        #region background setters
+
+        private static void background_main()
+        {
+            lock (_tcp_streams_mutex)
+            {
+                // Mock result
+                _tcp_streams.Add(new cTCPStream
+                {
+                    source_process_pid = "1337",
+                    source_process_name = "firefox.exe",
+                    source_ip = "127.0.0.1",
+                    destination_ip = "127.0.0.1",
+                    source_port = "2525",
+                    destination_port = "4454",
+                    backend_file = "127_0_0_1_2525_127_0_0_1_4454_201907100039.pcap",
+                    proxy_connected = false,
+                    stream_start = DateTime.Now.ToString(),
+                });
+
+                _tcp_streams.Add(new cTCPStream
+                {
+                    source_process_pid = "1337",
+                    source_process_name = "firefox.exe",
+                    source_ip = "127.0.0.1",
+                    destination_ip = "127.0.0.1",
+                    source_port = "2525",
+                    destination_port = "4457",
+                    backend_file = "127_0_0_1_2525_127_0_0_1_4454_201907100039.pcap",
+                    proxy_connected = true,
+                    stream_start = DateTime.Now.ToString(),
+                });
+            }
+
+            while (_background_is_running == true)
+            {
+                Thread.Sleep(100);
+            }
+        }
+
+        public static void background_thread_start()
+        {
+            if (_background_is_running == true)
+                throw new Exception("Error: background thread already running");
+
+            _background_is_running = true;
+
+            try
+            {
+                _background_thread = new Thread(background_main);
+                _background_thread.Start();
+            }
+            catch (Exception e)
+            {
+                _background_is_running = false;
+            }
+        }
+
+        public static void background_thread_stop()
+        {
+            if (_background_is_running == false)
+                throw new Exception("Error: background thread not running");
+
+            _background_thread.Join();
+        }
+
+        #endregion
+
+        #region UI getters
+
+        public static List<cTCPStream> ui_tcp_streams_get()
+        {
+            List<cTCPStream> result = new List<cTCPStream>();
+
+            lock (_tcp_streams_mutex)
+            {
+                result.AddRange(_tcp_streams);
+            }
+
+            // Mock results for now
+
+            return result;
+        }
+        #endregion
+
+    }
+}
+
